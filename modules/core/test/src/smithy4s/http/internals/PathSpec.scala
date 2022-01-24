8884c844d416a32f151eedfaeb1bfeaa875dcd9c
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-24 19:13:11 +0100

    Add some tests

--- modules/core/test/src/smithy4s/http/internals/PathSpec.scala
+++ modules/core/test/src/smithy4s/http/internals/PathSpec.scala
@@ -21,8 +21,26 @@ import smithy4s.example.DummyServiceGen.DummyPath
 import smithy4s.example.PathParams
 import smithy4s.http.HttpEndpoint
 import smithy4s.http.PathSegment
+import smithy4s.Schema
+import smithy.api.Http
+import smithy.api.NonEmptyString
 
 object PathSpec extends weaver.FunSuite {
+  import smithy4s.syntax._
+  object util {
+
+    def encodePathAs[A](schema: Schema[A]): Option[PathEncode[A]] = schema
+      .withHints(
+        Http(
+          method = NonEmptyString("GET"),
+          uri = NonEmptyString("/{label}")
+        )
+      )
+      .compile(SchematicPathEncoder)
+      .get
+
+    val simpleString = encodePathAs(string)
+  }
 
   test("Parse path pattern into path segments") {
     val result = pathSegments("/{head}/foo/{tail+}")
@@ -37,7 +55,7 @@ object PathSpec extends weaver.FunSuite {
     )
   }
 
-  test("Write PathParams") {
+  test("Write PathParams for DummyPath") {
     val result = HttpEndpoint
       .cast(
         DummyPath
@@ -62,6 +80,107 @@ object PathSpec extends weaver.FunSuite {
     }
 
     assert.eql(result, expected)
+  }
+
+  test("Write PathParams for a struct schema") {
+    val result = genericStruct(
+      Vector(
+        string.required[Unit]("label", _ => "example"),
+        string.required[Unit]("secondLabel", _ => "example2")
+      )
+    )(_ => ())
+      .withHints(
+        Http(
+          method = NonEmptyString("GET"),
+          uri = NonEmptyString("/{label}/const/{secondLabel}")
+        )
+      )
+      .compile(SchematicPathEncoder)
+      .get
+      .map(_.encode(()))
+
+    assert.eql(
+      result,
+      Some(List("example", "const", "example2"))
+    )
+  }
+
+  test("Write PathParams for a struct schema - URI ending with greedy label") {
+    val result = genericStruct(
+      Vector(
+        string.required[Unit]("label", _ => "example"),
+        string.required[Unit]("greedyLabel", _ => "example2/with/slashes")
+      )
+    )(_ => ())
+      .withHints(
+        Http(
+          method = NonEmptyString("GET"),
+          uri = NonEmptyString("/{label}/const/{greedyLabel+}")
+        )
+      )
+      .compile(SchematicPathEncoder)
+      .get
+      .map(_.encode(()))
+
+    assert.eql(
+      result,
+      Some(List("example", "const", "example2", "with", "slashes"))
+    )
+  }
+
+  test("Write PathParams for a simple string") {
+    assert.eql(
+      util.simpleString.map(_.encode("example")),
+      Some(List("example"))
+    )
+  }
+
+  test("Write PathParams for an int") {
+    assert.eql(
+      util.encodePathAs(int).map(_.encode(42)),
+      Some(List("42"))
+    )
+  }
+
+  test("Write PathParams for a double") {
+    assert.eql(
+      util.encodePathAs(double).map(_.encode(42.0)),
+      Some(List("42.0"))
+    )
+  }
+
+  test("Write PathParams for a boolean") {
+    assert.eql(
+      util.encodePathAs(boolean).map(_.encode(true)),
+      Some(List("true"))
+    )
+  }
+
+  test("Write PathParams for a string with special characters unencoded") {
+    val input = "example with all kinds of strange characters / \\ & "
+
+    assert.eql(
+      util.simpleString.map(_.encode(input)),
+      Some(List(input))
+    )
+  }
+
+  test(
+    "Write PathParams for a string greedily by splitting it on /, unencoded"
+  ) {
+    val input = "example/with/slashes and spaces"
+
+    assert.eql(
+      util.simpleString.map(_.encodeGreedy(input)),
+      Some(List("example", "with", "slashes and spaces"))
+    )
+  }
+
+  test("Write PathParams for unit as None") {
+    assert.eql(
+      util.encodePathAs(unit).map(_.encode(())),
+      None
+    )
   }
 
 }
