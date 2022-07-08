5f25805fb1646928347662a717d6704215b8df21
David Francoeur <dfrancoeur04@gmail.com>
2022-07-08 11:01:25 -0400

    Fix recursion issues and improve tests

--- modules/core/test/src/smithy4s/internal/SchemaDescriptionSpec.scala
+++ modules/core/test/src/smithy4s/internal/SchemaDescriptionSpec.scala
@@ -19,27 +19,73 @@ package internals
 
 import smithy4s.example._
 import munit._
+import org.scalacheck.Gen
+import org.scalacheck.Prop
+import org.scalacheck.Prop._
 
-class SchemaDescriptionSpec() extends FunSuite {
+class SchemaDescriptionSpec() extends FunSuite with ScalaCheckSuite { self =>
+  val genPrim: Gen[Schema[Any]] = Gen.oneOf[Schema[Any]](
+    Seq[Schema[_]](
+      Schema.short,
+      Schema.int,
+      Schema.long,
+      Schema.double,
+      Schema.float,
+      Schema.bigint,
+      Schema.bigdecimal,
+      Schema.string,
+      Schema.boolean,
+      Schema.byte,
+      Schema.bytes,
+      Schema.unit,
+      Schema.timestamp,
+      Schema.document,
+      Schema.uuid
+    ).map(_.asInstanceOf[Schema[Any]])
+  )
+
   def simple[A](s: Schema[A]): String = s.compile(SchemaDescription)
   def detailed[A](s: Schema[A]): String =
     s.compile(SchemaDescriptionDetailed)(Set.empty)._2
 
-  test("simple") {
-    assertEquals(simple(SomeInt.schema), "Int")
-    assertEquals(simple(SomeSet.schema), "Set")
-    assertEquals(simple(SomeList.schema), "List")
+  case class TestCase[A](schema: Schema[A], simple: String, detailed: String)
 
-    assertEquals(simple(BlobBody.schema), "Structure")
-    assertEquals(simple(IntList.schema), "Structure")
+  val testCases = Seq(
+    TestCase(SomeInt.schema, "Int", "Bijection { Int }"),
+    TestCase(SomeSet.schema, "Set", "Bijection { Set[String] }"),
+    TestCase(SomeList.schema, "List", "Bijection { List[String] }"),
+    TestCase(SomeMap.schema, "Map", "Bijection { Map[String, String] }"),
+    TestCase(
+      UntaggedUnion.schema,
+      "Union",
+      "Union { three: Bijection { Structure Three { three: String } }, four: Bijection { Structure Four { four: Int } } }"
+    ),
+    TestCase(
+      BlobBody.schema,
+      "Structure",
+      "Structure BlobBody { blob: Bytes }"
+    ),
+    TestCase(FooEnum.schema, "Enumeration", "Enumeration{ Foo }"),
+    TestCase(
+      IntList.schema,
+      "Structure",
+      "Structure IntList { head: Int, tail: Recursive { IntList } }"
+    ) // recursive
+  )
+
+  testCases.foreach { tc =>
+    test(s"simple ${tc.schema.shapeId}") {
+      assertEquals(simple(tc.schema), tc.simple)
+    }
+
+    test(s"detailed ${tc.schema.shapeId}") {
+      assertEquals(detailed(tc.schema), tc.detailed)
+    }
   }
 
-  test("detailed") {
-    assertEquals(detailed(SomeInt.schema), "Bijection { Int }")
-    assertEquals(detailed(SomeSet.schema), "Bijection { Set[String] }")
-    assertEquals(detailed(SomeList.schema), "Bijection { List[String] }")
-
-    assertEquals(detailed(BlobBody.schema), "Structure { blob: Bytes }") //
-    assertEquals(detailed(IntList.schema), "Structure") //
+  property("Check all primitive") {
+    forAll(genPrim) { (prim: Schema[Any]) =>
+      Prop(self.simple(prim) == self.detailed(prim))
+    }
   }
 }
