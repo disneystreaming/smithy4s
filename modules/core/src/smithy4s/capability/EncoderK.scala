c6709685475130f4b34e2a8e65ea93932634078c
Olivier Mélois <olivier.melois@disneystreaming.com>
2022-07-11 23:33:07 +0200

    Add EncoderK for function

--- modules/core/src/smithy4s/capability/EncoderK.scala
+++ modules/core/src/smithy4s/capability/EncoderK.scala
@@ -11,3 +11,11 @@ trait EncoderK[F[_], Result] extends Contravariant[F] {
   def contramap[A, B](fa: F[A])(f: B => A): F[B] =
     absorb[B](b => apply[A](fa, f(b)))
 }
+
+object EncoderK {
+  implicit def encoderKForFunction[B]: EncoderK[* => B, B] =
+    new EncoderK[* => B, B] {
+      def apply[A](fa: A => B, a: A): B = fa(a)
+      def absorb[A](f: A => B): A => B = f
+    }
+}
