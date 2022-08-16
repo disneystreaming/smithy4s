1adb13a5383fab285979eb2f8bb00cb10032bbfd
David Francoeur <dfrancoeur04@gmail.com>
2022-08-16 12:01:39 -0400

    DocumentKeyDecoder and DocumentKeyEncoder (#311)
    * Get rid of `canBeKey` in favor of a KeyEncoder
    * Get rid of `canBeKey` in favor of a KeyDecoder
    * Renamed to DocumentKey*
    * Refactor map implementation

---
+++ modules/core/src/smithy4s/internals/DocumentKeyDecoder.scala
@@ -0,0 +1,166 @@
+/*
+ *  Copyright 2021-2022 Disney Streaming
+ *
+ *  Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package smithy4s.internals
+
+import java.util.Base64
+import java.util.UUID
+
+import smithy4s._
+import smithy4s.Document._
+import smithy4s.schema.EnumValue
+import smithy4s.schema.Primitive
+import smithy4s.schema.Primitive._
+import smithy4s.schema.SchemaVisitor
+
+trait DocumentKeyDecoder[A] { self =>
+  def apply(v: Document): Either[DocumentKeyDecoder.DecodeError, A] =
+    try { Right(unsafeDecode(v)) }
+    catch { case ex: DocumentKeyDecoder.DecodeError => Left(ex) }
+
+  def unsafeDecode(v: Document): A
+
+  def map[B](f: A => B): DocumentKeyDecoder[B] = new DocumentKeyDecoder[B] {
+    def unsafeDecode(v: Document): B = f(self.unsafeDecode(v))
+  }
+}
+
+object DocumentKeyDecoder {
+  case class DecodeError(expectedType: String)
+      extends RuntimeException("Cannot decode a key.", null)
+
+  type OptDocumentKeyDecoder[A] = Option[DocumentKeyDecoder[A]]
+  val trySchemaVisitor: SchemaVisitor[OptDocumentKeyDecoder] =
+    new SchemaVisitor.Default[OptDocumentKeyDecoder] {
+      def default[A]: OptDocumentKeyDecoder[A] = None
+
+      def from[A](
+          expectedType: String
+      )(f: PartialFunction[Document, A]): OptDocumentKeyDecoder[A] =
+        Some { doc =>
+          if (f.isDefinedAt(doc)) f(doc)
+          else throw DecodeError(expectedType)
+        }
+      def fromUnsafe[A](
+          expectedType: String
+      )(f: PartialFunction[Document, A]): OptDocumentKeyDecoder[A] =
+        Some { doc =>
+          if (f.isDefinedAt(doc)) {
+            f(doc)
+          } else {
+            throw DecodeError(expectedType)
+          }
+        }
+
+      override def primitive[P](
+          shapeId: ShapeId,
+          hints: Hints,
+          tag: Primitive[P]
+      ): OptDocumentKeyDecoder[P] = {
+        val shortDesc = tag.schema(shapeId).compile(SchemaDescription)
+        tag match {
+          case PShort =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isValidShort => bd.shortValue
+            }
+          case PString => from(shortDesc) { case DString(value) => value }
+          case PFloat =>
+            from(shortDesc) { case FlexibleNumber(bd) =>
+              bd.toFloat
+            }
+          case PDouble =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isDecimalDouble => bd.toDouble
+            }
+
+          case PTimestamp => None // not sure because we can encode
+
+          case PBlob =>
+            fromUnsafe(shortDesc) { case DString(string) =>
+              ByteArray(Base64.getDecoder().decode(string))
+            }
+          case PBigInt =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isWhole => bd.toBigInt
+            }
+          case PUUID =>
+            fromUnsafe(shortDesc) { case DString(string) =>
+              UUID.fromString(string)
+            }
+          case PInt =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isValidInt => bd.intValue
+            }
+          case PBigDecimal =>
+            from(shortDesc) { case FlexibleNumber(bd) =>
+              bd
+            }
+          case PBoolean =>
+            from(shortDesc) {
+              case DBoolean(value)  => value
+              case DString("true")  => true
+              case DString("false") => false
+            }
+          case PLong =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isValidLong => bd.longValue
+            }
+          case PByte =>
+            from(shortDesc) {
+              case FlexibleNumber(bd) if bd.isValidByte => bd.toByte
+            }
+
+          case PUnit     => None
+          case PDocument => None
+        }
+      }
+      override def enumeration[E](
+          shapeId: ShapeId,
+          hints: Hints,
+          values: List[EnumValue[E]],
+          total: E => EnumValue[E]
+      ): OptDocumentKeyDecoder[E] = {
+        val fromName = values.map(e => e.stringValue -> e.value).toMap
+        from(s"value in [${fromName.keySet.mkString(", ")}]") {
+          case DString(value) if fromName.contains(value) => fromName(value)
+        }
+      }
+
+      override def biject[A, B](
+          schema: Schema[A],
+          to: A => B,
+          from: B => A
+      ): OptDocumentKeyDecoder[B] =
+        apply(schema).map(_.map(to))
+
+      override def surject[A, B](
+          schema: Schema[A],
+          to: Refinement[A, B],
+          from: B => A
+      ): OptDocumentKeyDecoder[B] =
+        apply(schema).map(_.map(to.asThrowingFunction))
+    }
+
+  object FlexibleNumber {
+    def unapply(doc: Document): Option[BigDecimal] = doc match {
+      case DNumber(value) => Some(value)
+      case DString(value) =>
+        try { Some(BigDecimal(value)) }
+        catch { case _: Throwable => None }
+      case _ => None
+    }
+  }
+}
---
+++ modules/core/src/smithy4s/internals/DocumentKeyEncoder.scala
@@ -0,0 +1,93 @@
+/*
+ *  Copyright 2021-2022 Disney Streaming
+ *
+ *  Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package smithy4s.internals
+
+import java.util.Base64
+
+import smithy.api.TimestampFormat
+import smithy.api.TimestampFormat._
+import smithy4s._
+import smithy4s.schema.EnumValue
+import smithy4s.schema.Primitive
+import smithy4s.schema.Primitive._
+import smithy4s.schema.SchemaVisitor
+
+trait DocumentKeyEncoder[A] {
+  def apply(a: A): String
+}
+
+object DocumentKeyEncoder {
+  type OptDocumentKeyEncoder[A] = Option[DocumentKeyEncoder[A]]
+  val trySchemaVisitor: SchemaVisitor[OptDocumentKeyEncoder] =
+    new SchemaVisitor.Default[OptDocumentKeyEncoder] {
+      private def instance[A](f: A => String): OptDocumentKeyEncoder[A] = Some {
+        a =>
+          a.toString()
+      }
+      private def forBigDecimal[A](
+          f: A => BigDecimal
+      ): OptDocumentKeyEncoder[A] =
+        Some { a =>
+          a.toString()
+        }
+      private def asString[A]: OptDocumentKeyEncoder[A] = instance {
+        _.toString()
+      }
+      def default[A]: OptDocumentKeyEncoder[A] = None
+
+      override def primitive[P](
+          shapeId: ShapeId,
+          hints: Hints,
+          tag: Primitive[P]
+      ): OptDocumentKeyEncoder[P] = {
+        tag match {
+          case PBoolean    => asString
+          case PBigDecimal => asString
+          case PUUID       => asString
+          case PString     => asString
+          case PShort      => forBigDecimal { a => BigDecimal(a.toInt) }
+          case PBigInt     => forBigDecimal { BigDecimal(_) }
+          case PInt        => forBigDecimal { BigDecimal(_) }
+          case PDouble     => forBigDecimal { BigDecimal(_) }
+          case PLong       => forBigDecimal { BigDecimal(_) }
+          case PByte       => forBigDecimal { a => BigDecimal(a.toInt) }
+          case PFloat      => forBigDecimal { a => BigDecimal(a.toDouble) }
+          case PBlob =>
+            instance(bytes => Base64.getEncoder().encodeToString(bytes.array))
+          case PUnit => None
+          case PTimestamp =>
+            hints
+              .get(TimestampFormat)
+              .getOrElse(DATE_TIME) match {
+              case DATE_TIME =>
+                instance { ts => ts.format(DATE_TIME) }
+              case HTTP_DATE =>
+                instance { ts => ts.format(HTTP_DATE) }
+              case EPOCH_SECONDS =>
+                forBigDecimal { ts => BigDecimal(ts.epochSecond) }
+            }
+          case PDocument => None
+        }
+      }
+      override def enumeration[E](
+          shapeId: ShapeId,
+          hints: Hints,
+          values: List[EnumValue[E]],
+          total: E => EnumValue[E]
+      ): OptDocumentKeyEncoder[E] = Some { a => total(a).stringValue }
+    }
+}
