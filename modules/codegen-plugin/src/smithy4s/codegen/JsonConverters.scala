7adbf1bed782f9547384453dc528fd6e2f5f9e53
Olivier Mélois <olivier.melois@disneystreaming.com>
2022-09-05 11:58:46 +0200

    Exhaustively capture the codegen input for cache invalidation
    This exhaustively captures the codegenArgs as a way to invalidate
    SBT's cache to retrigger codegen every time something changes.
    
    This uses SBT's JsonFormat, converting os-lib's paths into file hashes.
    In theory, when a local file changes (say, a dependency snapshot), the
    cache should now be invalidated and trigger a regeneration.

---
+++ modules/codegen-plugin/src/smithy4s/codegen/JsonConverters.scala
@@ -0,0 +1,69 @@
+package smithy4s.codegen
+
+import sjsonnew._
+import BasicJsonProtocol._
+import sbt.FileInfo
+import sbt.HashFileInfo
+import sjsonnew._, LList.:*:
+
+private[smithy4s] object JsonConverters {
+
+  implicit val pathFormat: JsonFormat[os.Path] = {
+    val hashInfoFormat = implicitly[JsonFormat[HashFileInfo]]
+    new JsonFormat[os.Path] {
+      def read[J](jsOpt: Option[J], unbuilder: Unbuilder[J]): os.Path = {
+        os.Path(hashInfoFormat.read(jsOpt, unbuilder).file)
+      }
+
+      def write[J](obj: os.Path, builder: Builder[J]): Unit =
+        hashInfoFormat.write(FileInfo.hash(obj.toIO), builder)
+    }
+  }
+
+  implicit val codegenArgsIso = LList.iso(
+    { ca: CodegenArgs =>
+      ("specs", ca.specs) :*:
+        ("output", ca.output) :*:
+        ("openapiOutput", ca.openapiOutput) :*:
+        ("skipScala", ca.skipScala) :*:
+        ("skipOpenapi", ca.skipOpenapi) :*:
+        ("discoverModels", ca.discoverModels) :*:
+        ("allowedNS", ca.allowedNS) :*:
+        ("excludedNS", ca.excludedNS) :*:
+        ("repositories", ca.repositories) :*:
+        ("dependencies", ca.dependencies) :*:
+        ("transformers", ca.transformers) :*:
+        ("localJars", ca.localJars) :*:
+        LNil
+    },
+    {
+      case (_, specs) :*:
+          (_, output) :*:
+          (_, openapiOutput) :*:
+          (_, skipScala) :*:
+          (_, skipOpenapi) :*:
+          (_, discoverModels) :*:
+          (_, allowedNS) :*:
+          (_, excludedNS) :*:
+          (_, repositories) :*:
+          (_, dependencies) :*:
+          (_, transformers) :*:
+          (_, localJars) :*: LNil =>
+        CodegenArgs(
+          specs,
+          output,
+          openapiOutput,
+          skipScala,
+          skipOpenapi,
+          discoverModels,
+          allowedNS,
+          excludedNS,
+          repositories,
+          dependencies,
+          transformers,
+          localJars
+        )
+    }
+  )
+
+}
