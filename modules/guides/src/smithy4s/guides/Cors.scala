b1f9fa9b2297482cf1b47102fcf0b46ccffb24cb
David Francoeur <dfrancoeur04@gmail.com>
2022-10-11 10:49:39 -0400

    Cleanup and add inline comments

--- modules/guides/src/smithy4s/guides/Cors.scala
+++ modules/guides/src/smithy4s/guides/Cors.scala
@@ -34,40 +34,58 @@ object HelloWorldImpl extends HelloWorldService[IO] {
   def sayWorld(): IO[World] = World().pure[IO]
 }
 
+/**
+  * This is an example of how you can configure CORS on a http4s router. The
+  * source of truth for the configuration is the Smithy annotation on the
+  * service. There is currently no support for CORS in Smithy4s because
+  * the `@cors` annotation is a too restrictive.
+  *
+  * See: https://github.com/awslabs/smithy/issues/1396
+  */
 object Routes {
   import org.http4s.server.middleware.*
 
-  println()
-
   val noMiddleware = (routes: HttpRoutes[IO]) => routes
 
-  val corsMiddleWare = HelloWorldServiceGen.hints
-    .get[smithy.api.Cors]
-    .map { corsConfig =>
-      val configuredOrigin = Origin.parse(corsConfig.origin.value)
-      configuredOrigin.swap.foreach { err =>
-        println(
-          s"Could not configure cors origin: ${err.getMessage()}"
-        )
-      }
-      val configuredExposedHeaders =
-        corsConfig.additionalExposedHeaders.toList.flatten
-          .map(_.value)
-          .toSet
-          .map(CIString(_))
-      val configuredAge =
-        Duration(corsConfig.maxAge, scala.concurrent.duration.SECONDS)
-      CORS.policy
-        .withAllowOriginHeader {
-          case Origin.Null => false
-          case o =>
-            configuredOrigin.toOption.exists { co => co == o }
-        }
-        .withExposeHeadersIn(configuredExposedHeaders)
-        .withAllowCredentials(false)
-        .withMaxAge(configuredAge)
-        .apply(_: HttpRoutes[IO])
+  /**
+    * We build a http4s CORS policy from the trait values.
+    * In this example, we set a few fields like the `origin`, the `maxAge`, etc
+    * but there is more information available in `smithy.api.Cors`.
+    *
+    * @param corsConfig instance of the trait with values from the specification
+    */
+  def buildHttp4sCors(corsConfig: smithy.api.Cors): CORSPolicy = {
+    val configuredOrigin = Origin.parse(corsConfig.origin.value)
+    configuredOrigin.swap.foreach { err =>
+      // There are better approach for error handling than a println but
+      // this will do for an example.
+      println(
+        s"Could not configure cors origin: ${err.getMessage()}"
+      )
     }
+    val configuredExposedHeaders =
+      corsConfig.additionalExposedHeaders.toList.flatten
+        .map(_.value)
+        .toSet
+        .map(CIString(_))
+    val configuredAge =
+      Duration(corsConfig.maxAge, scala.concurrent.duration.SECONDS)
+    CORS.policy
+      .withAllowOriginHeader {
+        case Origin.Null => false
+        case o =>
+          configuredOrigin.toOption.exists { co => co == o }
+      }
+      .withExposeHeadersIn(configuredExposedHeaders)
+      .withAllowCredentials(false)
+      .withMaxAge(configuredAge)
+  }
+
+  // We use Smithy4s api to retrieve the trait (called `Hint` in Smithy4s)
+  // We get an `Option` because service are not required to have this trait
+  val corsTrait = HelloWorldServiceGen.hints.get[smithy.api.Cors]
+  val corsMiddleWare = corsTrait
+    .map { corsConfig => buildHttp4sCors(corsConfig).apply(_: HttpRoutes[IO]) }
     .getOrElse(noMiddleware)
 
   private val helloRoutes: Resource[IO, HttpRoutes[IO]] =
