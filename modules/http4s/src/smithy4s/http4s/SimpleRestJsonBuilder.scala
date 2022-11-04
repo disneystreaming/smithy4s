ae94c4d62530ebd086ffe61c37c238b853a8df68
Yisraelu <ysrlunion@gmail.com>
2022-11-03 22:03:21 -0400

    adds abstract SimpleRestJsonBuilder layer to type the codec as

--- modules/http4s/src/smithy4s/http4s/SimpleRestJsonBuilder.scala
+++ modules/http4s/src/smithy4s/http4s/SimpleRestJsonBuilder.scala
@@ -19,12 +19,17 @@ package http4s
 
 import smithy4s.internals.InputOutput
 
-object SimpleRestJsonBuilder
+object SimpleRestJsonBuilder extends SimpleRestJsonBuilder(1024) {
+  def withMaxArity(maxArity: Int): SimpleRestJsonBuilder =
+    new SimpleRestJsonBuilder(maxArity) {}
+}
+abstract class SimpleRestJsonBuilder(maxArity: Int)
     extends SimpleProtocolBuilder[alloy.SimpleRestJson](
       smithy4s.http.json.codecs(
         alloy.SimpleRestJson.protocol.hintMask ++ HintMask(
           InputOutput,
           IntEnum
-        )
+        ),
+        maxArity
       )
     )
