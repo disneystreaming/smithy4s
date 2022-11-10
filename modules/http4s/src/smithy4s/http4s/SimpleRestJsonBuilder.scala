2d9ba1981d07cccca27f9640075f8ca474ce76d5
Yisraelu <ysrlunion@gmail.com>
2022-11-10 12:55:37 -0500

    moved maxArity method to class

--- modules/http4s/src/smithy4s/http4s/SimpleRestJsonBuilder.scala
+++ modules/http4s/src/smithy4s/http4s/SimpleRestJsonBuilder.scala
@@ -19,10 +19,8 @@ package http4s
 
 import smithy4s.internals.InputOutput
 
-object SimpleRestJsonBuilder extends SimpleRestJsonBuilder(1024) {
-  def withMaxArity(maxArity: Int): SimpleRestJsonBuilder =
-    new SimpleRestJsonBuilder(i)
-}
+object SimpleRestJsonBuilder extends SimpleRestJsonBuilder(1024)
+
 class SimpleRestJsonBuilder(maxArity: Int)
     extends SimpleProtocolBuilder[alloy.SimpleRestJson](
       smithy4s.http.json.codecs(
@@ -32,4 +30,7 @@ class SimpleRestJsonBuilder(maxArity: Int)
         ),
         maxArity
       )
-    )
+    ) {
+  def withMaxArity(maxArity: Int): SimpleRestJsonBuilder =
+    new SimpleRestJsonBuilder(maxArity)
+}
