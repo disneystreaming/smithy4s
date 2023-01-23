90429494dd486a81b83897c741e995d2c90c3e3f
Yisraelu <ysrlunion@gmail.com>
2023-01-23 11:34:00 -0500

    reimplement indexedSeq Eq because collection.IndexedSeq maps to mutable on Scala 2.12 and cats doesnt provide an instance of Eq for mutable Seq

--- modules/compliance-tests/src/smithy4s/compliancetests/internals/eq/Smithy4sEqInstances.scala
+++ modules/compliance-tests/src/smithy4s/compliancetests/internals/eq/Smithy4sEqInstances.scala
@@ -3,20 +3,20 @@ package smithy4s.compliancetests.internals.eq
 import cats.implicits.{catsSyntaxEq, toContravariantOps}
 import cats.kernel.Eq
 import smithy4s.{ByteArray, Document, Timestamp}
+import cats.kernel.instances.StaticMethods
 
 trait Smithy4sEqInstances {
   implicit def arrayEq[A: Eq]: Eq[Array[A]] = (x: Array[A], y: Array[A]) =>
     x.zip(y).forall { case (a, b) => a === b }
 
-  implicit def indexedSeq[A: Eq]: Eq[IndexedSeq[A]] =
-    Eq[Seq[A]].contramap(_.toSeq)
+  implicit def indexedSeqEq[A: Eq]: Eq[collection.IndexedSeq[A]] = (xs: collection.IndexedSeq[A], ys: collection.IndexedSeq[A]) => if (xs eq ys) true
+  else StaticMethods.iteratorEq(xs.iterator, ys.iterator)
 
   implicit val byteArrayEq: Eq[ByteArray] = (x: ByteArray, y: ByteArray) =>
     Eq[Array[Byte]].contramap[ByteArray](_.array).eqv(x, y)
-  implicit val documentEq: Eq[Document] =
-    implicitly[Eq[String]].contramap(_.show)
+  implicit val documentEq: Eq[Document] = Eq[String].contramap(_.show)
   implicit val timeStampEq: Eq[Timestamp] =
-    implicitly[Eq[Long]].contramap(_.epochSecond)
+    Eq[Long].contramap(_.epochSecond)
 
 }
 object Smithy4sEqInstances extends Smithy4sEqInstances
