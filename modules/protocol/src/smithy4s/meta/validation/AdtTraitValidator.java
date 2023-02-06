4cf276accdc20aeff795e744a4776d4d3652650b
Jeff Lewis <lewisjkl@me.com>
2023-02-06 15:09:22 -0700

    validator done, testing needed

--- modules/protocol/src/smithy4s/meta/validation/AdtTraitValidator.java
+++ modules/protocol/src/smithy4s/meta/validation/AdtTraitValidator.java
@@ -16,32 +16,35 @@
 
 package smithy4s.meta.validation;
 
-import smithy4s.meta.AdtMemberTrait;
+import smithy4s.meta.AdtTrait;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.shapes.Shape;
-import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.model.validation.AbstractValidator;
 import software.amazon.smithy.model.validation.ValidationEvent;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * All structures annotated with `@adtMember(SomeUnion)` are targeted in EXACTLY
- * ONE place: as a member of the union they reference in their idRef (SomeUnion
- * in this case)
+ * Unions marked with the adt trait must have at least one member. Also, the
+ * structures that the union targets must NOT be used within any other union.
  *
- * Also checks that structure is not empty (must have at least one member)
+ * Also checks that the structures targeted are not empty (they must have at
+ * least one member).
  */
-public final class AdtMemberTraitValidator extends AbstractValidator {
+public final class AdtTraitValidator extends AbstractValidator {
 
 	@Override
 	public List<ValidationEvent> validate(Model model) {
-		Set<Shape> adtMemberShapes = model.getShapesWithTrait(AdtMemberTrait.class);
-		Map<ShapeId, List<Shape>> grouped = adtMemberShapes.stream()
-				.collect(Collectors.groupingBy(mem -> mem.expectTrait(AdtMemberTrait.class).getValue()));
-		return grouped.entrySet().stream().flatMap(entry -> {
-			return AdtValidatorCommon.getReferenceEvents(model, entry.getValue(), model.expectShape(entry.getKey()));
+		return model.getShapesWithTrait(AdtTrait.class).stream().flatMap(adtShape -> {
+			Set<Shape> adtMemberShapes = new HashSet<>(adtShape.asUnionShape()
+					.orElseThrow(() -> new RuntimeException("adt trait may only be used on union shapes")).members());
+			if (adtMemberShapes.isEmpty()) {
+				return Stream.of(error(adtShape, "unions with the adt trait must contain at least one member"));
+			} else {
+				return AdtValidatorCommon.getReferenceEvents(model, adtMemberShapes, adtShape);
+			}
 		}).collect(Collectors.toList());
 	}
 }
