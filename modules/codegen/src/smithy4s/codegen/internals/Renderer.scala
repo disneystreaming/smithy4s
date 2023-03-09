7dfd037b0e1e26c5bfbdd7738b0c523810456c86
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-03-09 14:47:35 +0100

    Add logic to reduce collisions
    Add logic to prevent collisions shapes generated in other files, which
    Scala 3 seems to have trouble with when `"-source:3.0-migration"` is
    enabled

--- modules/codegen/src/smithy4s/codegen/internals/Renderer.scala
+++ modules/codegen/src/smithy4s/codegen/internals/Renderer.scala
@@ -81,8 +81,8 @@ private[internals] object Renderer {
       val renderResult = r.renderDecl(decl)
       val p = s"package ${unit.namespace}"
 
-      val nameCollisions: Set[String] = renderResult.list
-        .flatMap(_.segments.toList)
+      val segments = renderResult.list.flatMap(_.segments.toList)
+      val localCollisions: Set[String] = segments
         .groupBy {
           // we need to compare NameRefs as they would be imported in order to avoid unnecessarily qualifying types in the same package
           case ref: NameRef => ref.asImport
@@ -97,6 +97,19 @@ private[internals] object Renderer {
         .groupBy(identity)
         .filter(_._2.size > 1)
         .keySet
+
+      val otherDecls = unit.declarations.filterNot(_ == decl).map(_.name).toSet
+      def differentPackage(ref: NameRef): Boolean =
+        ref.pkg.mkString(".") != unit.namespace
+
+      // Collisions between references in the current file and declarations
+      // in the current package
+      val namespaceCollisions = segments.collect {
+        case ref: NameRef if otherDecls(ref.name) && differentPackage(ref) =>
+          ref.name
+      }.toSet
+
+      val nameCollisions = localCollisions ++ namespaceCollisions
 
       val allImports: List[String] = renderResult.list.flatMap { line =>
         line.segments.toList.collect {
--- modules/example/src/smithy4s/example/PersonAge.scala
+++ modules/example/src/smithy4s/example/PersonAge.scala
@@ -4,16 +4,15 @@ import smithy4s.Hints
 import smithy4s.Newtype
 import smithy4s.Schema
 import smithy4s.ShapeId
-import smithy4s.example.refined.Age
 import smithy4s.example.refined.Age.provider._
 import smithy4s.schema.Schema.bijection
 import smithy4s.schema.Schema.int
 
-object PersonAge extends Newtype[Age] {
+object PersonAge extends Newtype[smithy4s.example.refined.Age] {
   val id: ShapeId = ShapeId("smithy4s.example", "PersonAge")
   val hints: Hints = Hints(
     smithy.api.Default(smithy4s.Document.fromDouble(0.0d)),
   )
-  val underlyingSchema: Schema[Age] = int.refined[Age](smithy4s.example.AgeFormat()).withId(id).addHints(hints)
+  val underlyingSchema: Schema[smithy4s.example.refined.Age] = int.refined[smithy4s.example.refined.Age](smithy4s.example.AgeFormat()).withId(id).addHints(hints)
   implicit val schema: Schema[PersonAge] = bijection(underlyingSchema, asBijection)
 }
