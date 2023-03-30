90281f69ef74aee1f7d90f2d471b621800683d6f
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-03-30 16:23:33 +0200

    Headers and fmt

--- modules/http4s/src/smithy4s/http4s/SimpleProtocolCodecs.scala
+++ modules/http4s/src/smithy4s/http4s/SimpleProtocolCodecs.scala
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2021-2022 Disney Streaming
+ *
+ *  Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package smithy4s.http4s
 
 import cats.effect.Async
