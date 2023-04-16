2c046c807fcfa3d694d5a3b567dacd0c7e0c9265
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-04-16 03:11:52 +0200

    Update smithy-build, smithy-model to 1.30.0 (#903)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
   val Smithy = new {
     val org = "software.amazon.smithy"
-    val smithyVersion = "1.29.0"
+    val smithyVersion = "1.30.0"
     val model = org % "smithy-model" % smithyVersion
     val testTraits = org % "smithy-protocol-test-traits" % smithyVersion
     val build = org % "smithy-build" % smithyVersion
