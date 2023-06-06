516d65285e2274443e6a0ac8a672cfdd09f62f45
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-06-06 15:40:15 +0200

    Make 2.12 happy

--- modules/compliance-tests/src/smithy4s/compliancetests/ComplianceTest.scala
+++ modules/compliance-tests/src/smithy4s/compliancetests/ComplianceTest.scala
@@ -18,7 +18,6 @@ package smithy4s.compliancetests
 
 import ComplianceTest.ComplianceResult
 import smithy4s.ShapeId
-import smithy4s.compliancetests.TestConfig
 
 case class ComplianceTest[F[_]](
     id: String,
