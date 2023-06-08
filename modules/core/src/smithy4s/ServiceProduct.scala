d8be76689ac789d83716dd1e18b81a83143af202
miguel-vila <miguelvilag@gmail.com>
2023-06-08 17:23:22 +0100

    add scaladocs

--- modules/core/src/smithy4s/ServiceProduct.scala
+++ modules/core/src/smithy4s/ServiceProduct.scala
@@ -3,6 +3,16 @@ package smithy4s
 import smithy4s.kinds.FunctorK5
 import smithy4s.kinds.PolyFunction5
 
+/**
+  * Something that returns a product of endpoints.
+  * Contains the same information as a service, with the difference that the
+  * algebra (the product type parameter) can be be an interface with methods
+  * without inputs.
+  * 
+  * @tparam Prod the product type parameter. For code generation this is also
+  * generated as an interface with methods without inputs (one for each
+  * endpoint). This has suffix `ProductGen`.
+  */
 trait ServiceProduct[Prod[_[_, _, _, _, _]]] extends FunctorK5[Prod] {
   type Alg[_[_, _, _, _, _]]
   val service: Service[Alg]
@@ -22,6 +32,12 @@ object ServiceProduct {
 
 }
 
+/**
+  * Provides a way to get the product version for a service.
+  * For info about the `Prod` type parameter, see [[ServiceProduct]].
+  * 
+  * @tparam Alg the algebra type parameter.
+  */
 trait Mirror[Alg[_[_, _, _, _, _]]] {
   type Prod[_[_, _, _, _, _]]
   val serviceProduct: ServiceProduct.Aux[Prod, Alg]
