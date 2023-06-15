95fc8c38a17369f9f979a3849578faf2912df212
miguel-vila <miguelvilag@gmail.com>
2023-06-15 12:04:25 +0100

    remove dynamic changes

--- modules/dynamic/src/smithy4s/dynamic/internals/DynamicService.scala
+++ modules/dynamic/src/smithy4s/dynamic/internals/DynamicService.scala
@@ -19,7 +19,6 @@ package dynamic
 package internals
 
 import smithy4s.kinds.PolyFunction5
-import smithy4s.ServiceProduct
 
 private[internals] case class DynamicService(
     id: ShapeId,
@@ -29,67 +28,20 @@ private[internals] case class DynamicService(
 ) extends Service.Reflective[DynamicOp]
     with DynamicSchemaIndex.ServiceWrapper {
 
+  type Alg[P[_, _, _, _, _]] = PolyFunction5.From[DynamicOp]#Algebra[P]
+  override val service: Service[Alg] = this
+
   private lazy val endpointMap: Map[ShapeId, Endpoint[_, _, _, _, _]] =
     endpoints.map(ep => ep.id -> ep).toMap
-
-  def getEndpoint[I, E, O, SI, SO](
-      id: ShapeId
-  ): Endpoint[I, E, O, SI, SO] =
-    endpointMap
-      .getOrElse(id, sys.error("Unknown endpoint: " + id))
-      .asInstanceOf[Endpoint[I, E, O, SI, SO]]
 
   def endpoint[I, E, O, SI, SO](
       op: DynamicOp[I, E, O, SI, SO]
   ): (I, Endpoint[I, E, O, SI, SO]) = {
-    val endpoint = getEndpoint[I, E, O, SI, SO](op.id)
+    val endpoint = endpointMap
+      .getOrElse(op.id, sys.error("Unknown endpoint: " + op.id))
+      .asInstanceOf[Endpoint[I, E, O, SI, SO]]
     val input = op.data
     (input, endpoint)
   }
-
-  type Alg[P[_, _, _, _, _]] = PolyFunction5[DynamicOp, P]
-
-  override val service: Service[Alg] = this
-
-  type Prod[P[_, _, _, _, _]] = PolyFunction5[NoInputOp, P]
-
-  val serviceProduct
-      : ServiceProduct.Aux[Prod, PolyFunction5.From[DynamicOp]#Algebra] =
-    new DynamicServiceProduct(
-      this
-    )
-
-}
-
-private[internals] class DynamicServiceProduct(
-    override val service: DynamicService
-) extends ServiceProduct[PolyFunction5.From[NoInputOp]#Algebra] {
-
-  type Alg[F[_, _, _, _, _]] = PolyFunction5[DynamicOp, F]
-
-  override def mapK5[F[_, _, _, _, _], G[_, _, _, _, _]](
-      alg: PolyFunction5[NoInputOp, F],
-      function: PolyFunction5[F, G]
-  ): PolyFunction5[NoInputOp, G] =
-    alg.andThen(function)
-
-  override val endpointsProduct: PolyFunction5[NoInputOp, service.Endpoint] =
-    new PolyFunction5[NoInputOp, service.Endpoint] {
-      override def apply[I, E, O, SI, SO](
-          op: NoInputOp[I, E, O, SI, SO]
-      ): Endpoint[DynamicOp, I, E, O, SI, SO] =
-        service.getEndpoint(op.id)
-    }
-
-  override def toPolyFunction[P2[_, _, _, _, _]](
-      algebra: PolyFunction5[NoInputOp, P2]
-  ): PolyFunction5[service.Endpoint, P2] =
-    new PolyFunction5[service.Endpoint, P2] {
-      override def apply[I, E, O, SI, SO](
-          endpoint: smithy4s.Endpoint[DynamicOp, I, E, O, SI, SO]
-      ): P2[I, E, O, SI, SO] = {
-        algebra.apply(NoInputOp(endpoint.id))
-      }
-    }
 
 }
