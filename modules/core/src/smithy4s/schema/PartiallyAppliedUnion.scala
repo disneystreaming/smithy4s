49b1abec07b6c9bf3e32ea4a90bc30aaee66a93b
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-07-17 12:33:58 +0200

    cleanup

--- modules/core/src/smithy4s/schema/PartiallyAppliedUnion.scala
+++ modules/core/src/smithy4s/schema/PartiallyAppliedUnion.scala
@@ -7,17 +7,4 @@ class PartiallyAppliedUnion[U](val alts: Vector[Alt[U, _]]) extends AnyVal {
   def apply(f: U => Int): Schema.UnionSchema[U] =
     Schema.UnionSchema(Schema.placeholder, Hints.empty, alts, f)
 
-  // def apply(
-  //     dispatch: U => Alt.WithValue[U, _],
-  //     dummy: Boolean = false // source-compat since 0.18
-  // ): Schema.UnionSchema[U] = {
-  //   val indexMap = alts.zipWithIndex.toMap
-  //   Schema.UnionSchema(
-  //     Schema.placeholder,
-  //     Hints.empty,
-  //     alts,
-  //     (u: U) => indexMap(dispatch(u).alt)
-  //   )
-  // }
-
 }
