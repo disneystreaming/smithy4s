f059bc1224aaf4b91ca82893766b7049c634d76e
Jeff Lewis <lewisjkl@me.com>
2023-09-20 14:49:48 -0600

    Fix for Json Codec Cursor

---
+++ modules/json/test/src/smithy4s/json/internals/CursorSpec.scala
@@ -0,0 +1,20 @@
+package smithy4s.json.internals
+
+import munit._
+import smithy4s.json.internals.Cursor
+import smithy4s.codecs.PayloadPath
+
+final class CursorSpec extends FunSuite {
+
+  test("check") {
+    val c = new Cursor()
+    c.push("test")
+    c.pop()
+    c.push(1)
+    assertEquals(
+      c.getPath(List.empty),
+      PayloadPath(List(PayloadPath.Segment(1)))
+    )
+  }
+
+}
