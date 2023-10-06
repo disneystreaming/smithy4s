546b5871a220c5b43f9cbb472f723d060357da12
Scala Steward <scala_steward@virtuslab.com>
2023-10-06 14:40:53 +0000

    Update fs2-data-xml to 1.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
 
   object Fs2Data {
     val xml: Def.Initialize[ModuleID] =
-      Def.setting("org.gnieh" %%% "fs2-data-xml" % "1.8.1")
+      Def.setting("org.gnieh" %%% "fs2-data-xml" % "1.9.0")
   }
 
   object Mill {
