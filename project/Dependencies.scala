15722b15903d74b73fbcd56398ebb1d3b1be94fd
Scala Steward <scala_steward@virtuslab.com>
2023-11-09 16:49:58 +0000

    Update fs2-core, fs2-io to 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
       Def.setting("com.monovore" %%% "decline-effect" % declineVersion)
   }
   object Fs2 {
-    val fs2Version = "3.9.2"
+    val fs2Version = "3.9.3"
 
     val core: Def.Initialize[ModuleID] =
       Def.setting("co.fs2" %%% "fs2-core" % fs2Version)
