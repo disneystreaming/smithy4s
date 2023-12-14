4beaa9b416513edde6fcb01beb26b620d99fa6c7
Jakub Kozłowski <kubukoz@gmail.com>
2023-12-14 02:15:18 +0100

    Remove test ignore

--- modules/bootstrapped/test/src/smithy4s/DocumentSpec.scala
+++ modules/bootstrapped/test/src/smithy4s/DocumentSpec.scala
@@ -24,8 +24,7 @@ import munit._
 
 class DocumentSpec() extends FunSuite {
 
-  // fails with an SOE
-  test("Recursive document codecs should not blow up the stack".ignore) {
+  test("Recursive document codecs should not blow up the stack") {
     val recursive: IntList = IntList(1, Some(IntList(2, Some(IntList(3)))))
 
     val document = Document.encode(recursive)
