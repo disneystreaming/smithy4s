fbb4d2ce04fc4e5a5c9b100fd355a9bc9a5bcf6c
Scala Steward <scala_steward@virtuslab.com>
2024-01-16 15:48:09 +0000

    Update swagger-ui-dist to 5.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -148,7 +148,7 @@ object Dependencies {
   val Slf4jSimple = "org.slf4j" % "slf4j-simple" % "2.0.11"
 
   object Webjars {
-    val swaggerUi: ModuleID = "org.webjars.npm" % "swagger-ui-dist" % "5.9.4"
+    val swaggerUi: ModuleID = "org.webjars.npm" % "swagger-ui-dist" % "5.11.0"
 
     val webjarsLocator: ModuleID = "org.webjars" % "webjars-locator" % "0.50"
   }
