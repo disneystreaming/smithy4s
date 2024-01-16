cac21995e58cd73c78a7956d0e3ab229fa448e7e
Scala Steward <scala_steward@virtuslab.com>
2024-01-16 15:47:57 +0000

    Update cats-effect to 3.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -98,7 +98,7 @@ object Dependencies {
    * modules/tests/src-ce2/UUIDGen.scala
    */
   val CatsEffect3: Def.Initialize[ModuleID] =
-    Def.setting("org.typelevel" %%% "cats-effect" % "3.5.2")
+    Def.setting("org.typelevel" %%% "cats-effect" % "3.5.3")
 
   object Http4s {
     val http4sVersion = "0.23.25"
