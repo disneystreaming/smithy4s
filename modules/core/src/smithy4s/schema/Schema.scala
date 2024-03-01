62c294fdc647475b3eef4c6f2a42ed8499aae4ff
Jakub Kozłowski <kubukoz@gmail.com>
2024-03-01 15:02:19 +0100

    Add transformTransitivelyK

--- modules/core/src/smithy4s/schema/Schema.scala
+++ modules/core/src/smithy4s/schema/Schema.scala
@@ -190,6 +190,55 @@ object Schema {
     def apply[A](fa: Schema[A]): Schema[A] = fa.transformHintsTransitively(f)
   }
 
+  /**
+   * Transforms this schema, and all the schemas inside it, using the provided function.
+   */
+  def transformTransitivelyK(f: Schema ~> Schema): Schema ~> Schema = new (Schema ~> Schema) {
+    private val compiler = new TransitiveCompiler(f)
+
+    def apply[A](fa: Schema[A]): Schema[A] = fa.compile(compiler)
+  }
+
+  // format: on
+  private final class TransitiveCompiler(
+      underlying: Schema ~> Schema
+  ) extends (Schema ~> Schema) {
+
+    def apply[A](
+        fa: Schema[A]
+    ): Schema[A] = fa match {
+      case e @ EnumerationSchema(_, _, _, _, _) => underlying(e)
+      case p @ PrimitiveSchema(_, _, _)         => underlying(p)
+      case u @ UnionSchema(_, _, _, _) =>
+        underlying(u.copy(alternatives = u.alternatives.map(handleAlt(_))))
+      case BijectionSchema(s, bijection) =>
+        underlying(BijectionSchema(this(s), bijection))
+      case LazySchema(suspend) =>
+        underlying(LazySchema(suspend.map(this.apply)))
+      case RefinementSchema(s, refinement) =>
+        underlying(RefinementSchema(this(s), refinement))
+      case c: CollectionSchema[c, a] =>
+        underlying(c.copy(member = this(c.member)))
+      case m @ MapSchema(_, _, _, _) =>
+        underlying(m.copy(key = this(m.key), value = this(m.value)))
+      case s @ StructSchema(_, _, _, _) =>
+        underlying(s.copy(fields = s.fields.map(handleField(_))))
+      case n @ OptionSchema(_) =>
+        underlying(n.copy(underlying = this(n.underlying)))
+    }
+
+    private def handleField[S, A](
+        field: Field[S, A]
+    ): Field[S, A] = field.copy(schema = this(field.schema))
+
+    private def handleAlt[S, A](
+        alt: Alt[S, A]
+    ): Alt[S, A] = alt.copy(schema = this(alt.schema))
+  }
+
+  // format: off
+
+
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // SCHEMA BUILDER
   //////////////////////////////////////////////////////////////////////////////////////////////////
