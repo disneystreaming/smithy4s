1c40c45487ffa36e4ba84f9dcd7c4db5e1b06753
David Francoeur <dfrancoeur04@gmail.com>
2024-05-05 15:51:00 -0400

    Filter mixins that have different types
    Similar to what was done in #425, I've filtered out the
    mixins that have different type than the final shape they're
    applied to.
    
    As such, in the example above, `HasName` has a optional name
    member where as `Person` inherits the mixin but makes the member
    required. The filtering catches that and as a result the mixin is
    removed from the list of mixins to add on `Person`.
    
    This behaviour aligns with the behaviour that happens when no `@adt`
    trait is added and `Person` is code generated on it's own. In both
    cases, now, the trait `HasName` is not extended because the types of
    `name` don't align.

---
+++ modules/bootstrapped/src/generated/smithy4s/example/PersonUnion.scala
@@ -0,0 +1,67 @@
+package smithy4s.example
+
+import smithy4s.Hints
+import smithy4s.Schema
+import smithy4s.ShapeId
+import smithy4s.ShapeTag
+import smithy4s.schema.Schema.string
+import smithy4s.schema.Schema.struct
+import smithy4s.schema.Schema.union
+
+sealed trait PersonUnion extends scala.Product with scala.Serializable { self =>
+  @inline final def widen: PersonUnion = this
+  def $ordinal: Int
+
+  object project {
+    def p: Option[PersonUnion.OtherPerson] = PersonUnion.OtherPerson.alt.project.lift(self)
+  }
+
+  def accept[A](visitor: PersonUnion.Visitor[A]): A = this match {
+    case value: PersonUnion.OtherPerson => visitor.p(value)
+  }
+}
+object PersonUnion extends ShapeTag.Companion[PersonUnion] {
+
+  def otherPerson(name: String):OtherPerson = OtherPerson(name)
+
+  val id: ShapeId = ShapeId("smithy4s.example", "PersonUnion")
+
+  val hints: Hints = Hints.empty
+
+  final case class OtherPerson(name: String) extends PersonUnion {
+    def $ordinal: Int = 0
+  }
+
+  object OtherPerson extends ShapeTag.Companion[OtherPerson] {
+    val id: ShapeId = ShapeId("smithy4s.example", "OtherPerson")
+
+    val hints: Hints = Hints.empty
+
+    // constructor using the original order from the spec
+    private def make(name: String): OtherPerson = OtherPerson(name)
+
+    val schema: Schema[OtherPerson] = struct(
+      string.required[OtherPerson]("name", _.name),
+    )(make).withId(id).addHints(hints)
+
+    val alt = schema.oneOf[PersonUnion]("p")
+  }
+
+
+  trait Visitor[A] {
+    def p(value: PersonUnion.OtherPerson): A
+  }
+
+  object Visitor {
+    trait Default[A] extends Visitor[A] {
+      def default: A
+      def p(value: PersonUnion.OtherPerson): A = default
+    }
+  }
+
+  implicit val schema: Schema[PersonUnion] = union(
+    PersonUnion.OtherPerson.alt,
+  ){
+    _.$ordinal
+  }.withId(id).addHints(hints)
+}
