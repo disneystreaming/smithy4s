18adf2223c0384d76dcb320375b2a1f6f2ed6125
Jakub Kozłowski <kubukoz@gmail.com>
2024-09-06 14:13:47 +0200

    bump scalas

--- project/Smithy4sBuildPlugin.scala
+++ project/Smithy4sBuildPlugin.scala
@@ -29,8 +29,8 @@ case class CatsEffectAxis(idSuffix: String, directorySuffix: String)
 
 object Smithy4sBuildPlugin extends AutoPlugin {
 
-  val Scala212 = "2.12.19"
-  val Scala213 = "2.13.13"
+  val Scala212 = "2.12.20"
+  val Scala213 = "2.13.14"
   val Scala3 = "3.3.3"
 
   object autoImport {
