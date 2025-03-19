e13eff04f6d286c48e9257689f109f6e90093b7a
Jakub Kozłowski <kubukoz@gmail.com>
2025-03-19 22:53:26 +0100

    Add new suite

---
+++ modules/bootstrapped/test/src/smithy4s/FieldFilterSpec.scala
@@ -0,0 +1,37 @@
+package smithy4s
+
+import munit.FunSuite
+import smithy4s.schema.FieldFilter
+import smithy4s.schema.Field
+import smithy4s.Schema
+import munit.Location
+
+class FieldFilterSpec extends FunSuite {
+
+  test("SkipUnsetOptions should remove Nones") {
+    check(Schema.string.option, None, false)
+  }
+
+  test("SkipUnsetOptions should remove Nones bijected to something else") {
+    case class OptionalLike[+A](underlying: Option[A])
+
+    check(
+      Schema.string.option.biject(OptionalLike(_))(_.underlying),
+      OptionalLike(None),
+      false
+    )
+  }
+
+  test("SkipUnsetOptions should keep null nullables") {
+    check(Schema.string.nullable, Nullable.Null, true)
+  }
+
+  private def check[A](schema: Schema[A], value: A, expectedToKeep: Boolean)(
+      implicit loc: Location
+  ) = {
+    val filter =
+      FieldFilter.SkipUnsetOptions.compile(Field("label", schema, identity[A]))
+    assertEquals(filter(value), expectedToKeep)
+  }
+
+}
