affbe1cb3222c8fe9610b2ce51f3d8c724bbda2b
Jakub Kozłowski <kubukoz@gmail.com>
2025-04-02 22:44:00 +0200

    Use special alloy version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
 
   val Alloy = new {
     val org = "com.disneystreaming.alloy"
-    val alloyVersion = "0.13.17-SNAPSHOT"
+    val alloyVersion = "0.3.16-13-5640c6-SNAPSHOT"
     val core = org % "alloy-core" % alloyVersion
     val openapi = org %% "alloy-openapi" % alloyVersion
     val protobuf = org % "alloy-protobuf" % alloyVersion
