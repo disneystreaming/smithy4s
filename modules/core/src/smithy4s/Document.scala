c243c7a1f748ae493bbec70d73f66751205d000c
Jakub Kozłowski <kubukoz@gmail.com>
2025-04-29 00:44:33 +0200

    Real scaladoc

--- modules/core/src/smithy4s/Document.scala
+++ modules/core/src/smithy4s/Document.scala
@@ -63,7 +63,10 @@ sealed trait Document extends Product with Serializable {
       value.map { case (k, v) => k + "=" + v.show }.mkString("{", ", ", "}")
   }
 
-  // Nests this document under the given key.
+  /**
+    * Nests this document under the given key.
+    * The result is a single-entry Document object.
+    */
   def nest(k: String): Document = obj(k -> this)
 }
 
