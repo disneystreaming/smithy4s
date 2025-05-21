95899f093f8b0c658c918a096456512b65a80754
yisraelu <ysrlunion@gmail.com>
2025-05-21 14:40:27 -0400

    add mapping between java.net.URI and HttpUri

--- modules/core/src/smithy4s/http/HttpUri.scala
+++ modules/core/src/smithy4s/http/HttpUri.scala
@@ -15,6 +15,7 @@
  */
 
 package smithy4s.http
+import java.net.URI
 
 /**
  * RFC 3986 compliant URI implementation.
@@ -36,6 +37,24 @@ final case class HttpUri private (
       */
     pathParams: Option[Map[String, String]]
 ) {
+  def toURI: URI = {
+    val schemeStr = scheme match {
+      case HttpUriScheme.Http  => "http"
+      case HttpUriScheme.Https => "https"
+    }
+    val portStr = port.map(":" + _).getOrElse("")
+    val pathStr = path.mkString("/", "/", "")
+    val queryStr =
+      if (queryParams.isEmpty) ""
+      else
+        "?" + queryParams
+          .map { case (k, v) =>
+            v.map(vv => s"$k=$vv").mkString("&")
+          }
+          .mkString("&")
+    new URI(s"$schemeStr://$host$portStr$pathStr$queryStr")
+  }
+}
 
   def authority: Option[HttpUriAuthority] = origin.map(_.authority)
 
@@ -164,6 +183,33 @@ object HttpUri {
       pathParams: Option[Map[String, String]]
   ): HttpUri = {
     new HttpUri(origin, path, queryParams, pathParams)
+  }
+    def fromURI(uri: URI): HttpUri = {
+      val scheme = uri.getScheme() match {
+        case "https" => HttpUriScheme.Https
+        case _       => HttpUriScheme.Http
+      }
+      val host = uri.getHost()
+      val port = Option(uri.getPort()).filter(_ >= 0)
+      val path = uri.getPath.split('/').filter(_.nonEmpty).toIndexedSeq
+      val queryParams: Map[String, Seq[String]] = Option(uri.getQuery())
+        .map { query =>
+          query
+            .split("&")
+            .map { pair =>
+              pair.split("=") match {
+                case Array(k: String, v: String) => k -> Seq(v)
+                case Array(k: String)            => k -> Seq.empty[String]
+                // cases where you have q1=v1=v2 => q1 -> "v1=v2"
+                case v @ Array(k: String, _*) => k -> Seq(v.tail.mkString("="))
+              }
+            }
+            .groupBy(_._1)
+            .map { case (k, vs) => k -> vs.map(_._2).flatten.toSeq }
+        }
+        .getOrElse(Map.empty)
+      HttpUri(scheme, host, port, path, queryParams, None)
+    }
   }
 
   /**
