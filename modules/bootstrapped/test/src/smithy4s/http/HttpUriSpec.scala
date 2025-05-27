4a89d12b532803cb94777894c3f1b80621913032
yisraelu <ysrlunion@gmail.com>
2025-05-27 13:38:17 -0400

    fix uri encoding issue for conversion to java.net.URI , plus add tests

--- modules/bootstrapped/test/src/smithy4s/http/HttpUriSpec.scala
+++ modules/bootstrapped/test/src/smithy4s/http/HttpUriSpec.scala
@@ -39,4 +39,30 @@ final class HttpUriSpec extends FunSuite {
     val uri = httpUri.toURI
     assertEquals(httpUri, HttpUri.fromURI(uri))
   }
+
+  test("Roundtrip preserves uri encoding from java.net.URI") {
+    // This URI contains spaces, which should be encoded as %20
+    val uri = URI.create("http://example.com/foo%20bar?baz=qux%20quux")
+    val httpUri = HttpUri.fromURI(uri)
+    assertEquals(uri, httpUri.toURI)
+    assert(httpUri.path == IndexedSeq("foo bar"))
+    assert(httpUri.queryParams == Map("baz" -> List("qux quux")))
+  }
+  test("Roundtrip preserves uri encoding from HttpUri") {
+    // This HttpUri contains spaces, which should be encoded as %20, however HttpUri stores data in its decoded form
+    // so we expect the spaces to be present in the path and query parameters.
+    val httpUri = HttpUri(
+      HttpUriScheme.Http,
+      "example.com",
+      None,
+      IndexedSeq("foo bar"),
+      Map("baz" -> List("qux quux")),
+      Option.empty
+    )
+    val uri = httpUri.toURI
+    assertEquals(httpUri, HttpUri.fromURI(uri))
+    assert(uri.getRawPath == "/foo%20bar")
+    assert(uri.getRawQuery == "baz=qux%20quux")
+  }
+
 }
