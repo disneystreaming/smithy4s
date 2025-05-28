b057ed6a368ae003f63ec42ec1c5f337ac69560a
Jakub Kozłowski <kubukoz@gmail.com>
2025-05-28 18:48:32 +0200

    Update modules/bootstrapped/test/src/smithy4s/http/HttpUriSpec.scala

--- modules/bootstrapped/test/src/smithy4s/http/HttpUriSpec.scala
+++ modules/bootstrapped/test/src/smithy4s/http/HttpUriSpec.scala
@@ -61,7 +61,7 @@ final class HttpUriSpec extends FunSuite {
     if (Platform.isNative)
       assume(
         false,
-        "This test is not applicable for Scala Native, as we are on 4.x which has bugs in URI parsing and encoding"
+        "This test is not applicable for Scala Native, as we are on 0.4.x which has bugs in URI parsing and encoding"
       )
     else {
       // This HttpUri contains spaces, which should be encoded as %20, however HttpUri stores data in its decoded form
