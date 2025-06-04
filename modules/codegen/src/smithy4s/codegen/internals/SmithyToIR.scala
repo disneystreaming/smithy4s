8f1b22fd05d9b88bdba7db9fa4d3895f8fe151e8
Jakub Kozłowski <kubukoz@gmail.com>
2025-06-04 20:15:27 +0200

    format

--- modules/codegen/src/smithy4s/codegen/internals/SmithyToIR.scala
+++ modules/codegen/src/smithy4s/codegen/internals/SmithyToIR.scala
@@ -141,7 +141,7 @@ private[codegen] class SmithyToIR(
   def allDecls = allShapes
     .filter(_.getId().getNamespace() == namespace)
     // Only structure mixins should be generated
-    .filterNot { s => s.hasTrait(classOf[MixinTrait]) && !s.isStructureShape()}
+    .filterNot { s => s.hasTrait(classOf[MixinTrait]) && !s.isStructureShape() }
     .flatMap(_.accept(toIRVisitor(renderAdtMemberStructures = false)))
     .toList
 
