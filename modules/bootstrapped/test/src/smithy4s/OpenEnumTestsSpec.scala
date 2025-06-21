77b650294029333a7920827e83edaa08a536d3c2
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-06-21 13:31:31 +0000

    sbt scalafmtAll

--- modules/bootstrapped/test/src/smithy4s/OpenEnumTestsSpec.scala
+++ modules/bootstrapped/test/src/smithy4s/OpenEnumTestsSpec.scala
@@ -6,12 +6,18 @@ class OpenEnumTestsSpec extends FunSuite {
 
   test("OpenEnumTest uses fromStringOrUnknown for known and unknown values") {
     assertEquals(OpenEnumTest.fromStringOrUnknown("ONE"), OpenEnumTest.ONE)
-    assertEquals(OpenEnumTest.fromStringOrUnknown("UNKNOWN_VALUE"), OpenEnumTest.$Unknown("UNKNOWN_VALUE"))
+    assertEquals(
+      OpenEnumTest.fromStringOrUnknown("UNKNOWN_VALUE"),
+      OpenEnumTest.$Unknown("UNKNOWN_VALUE")
+    )
   }
 
   test("OpenIntEnumTest uses fromIntOrUnknown for known and unknown values") {
     assertEquals(OpenIntEnumTest.fromIntOrUnknown(1), OpenIntEnumTest.ONE)
-    assertEquals(OpenIntEnumTest.fromIntOrUnknown(999), OpenIntEnumTest.$Unknown(999))
+    assertEquals(
+      OpenIntEnumTest.fromIntOrUnknown(999),
+      OpenIntEnumTest.$Unknown(999)
+    )
   }
 
 }
