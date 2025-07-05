f055cfd1b5f2271c4613bd6f17b02d4bb3bdf96b
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-07-05 07:24:57 +0000

    Add smithy4s.dynamic.syntax with public Document conversions

---
+++ modules/dynamic/src-jvm/syntax.scala
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2021-2025 Disney Streaming
+ *
+ *  Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package smithy4s.dynamic
+
+import smithy4s.{Document, ShapeId, Hints}
+import software.amazon.smithy.model.node.Node
+import software.amazon.smithy.model.shapes.{ShapeId => SmithyShapeId}
+import software.amazon.smithy.model.traits.Trait
+import scala.jdk.CollectionConverters._
+
+object syntax {
+  def documentToNode(doc: Document): Node = doc match {
+    case DString(value)  => Node.from(value)
+    case DNumber(value)  => Node.from(value)
+    case DBoolean(value) => Node.from(value)
+    case DObject(values) =>
+      Node.objectNode(values.map { case (k, v) =>
+        Node.from(k) -> documentToNode(v)
+      }.asJava)
+    case DArray(values) => Node.fromNodes(values.map(documentToNode): _*)
+    case DNull          => Node.nullNode()
+  }
+
+  def nodeToDocument(node: Node): Document = NodeToDocument(node)
+
+}
