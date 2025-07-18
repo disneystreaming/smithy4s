c4228c390122cc2f85ee9a921c149e3b634ef950
Antonio Morales <Antonio.Morales.-ND@disney.com>
2025-07-18 10:55:36 -0700

    update dynamic module

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
 
   val Alloy = new {
     val org = "com.disneystreaming.alloy"
-    val alloyVersion = "0.3.28"
+    val alloyVersion = "dev-SNAPSHOT"
     val core = org % "alloy-core" % alloyVersion
     val openapi = org %% "alloy-openapi" % alloyVersion
     val protobuf = org % "alloy-protobuf" % alloyVersion
