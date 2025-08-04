9ddd935efa7f268a44acfa240d1447d891f803c1
msosnicki <msosnicki87@gmail.com>
2025-08-04 16:58:20 +0200

    Header

--- modules/benchmark/src/main/scala/UnionJsonBenchmark.scala
+++ modules/benchmark/src/main/scala/UnionJsonBenchmark.scala
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2021-2025 Disney Streaming
+ *
+ *  Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package smithy4s.benchmark
 
 import org.openjdk.jmh.annotations.BenchmarkMode
@@ -16,10 +32,10 @@ import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.infra.Blackhole
 import smithy4s.Blob
 
-@BenchmarkMode(Array(Mode.AverageTime))
+@BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 20, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 20, time = 30, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 50, time = 30, timeUnit = TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 @Fork(3)
 class UnionJsonBenchmark {
