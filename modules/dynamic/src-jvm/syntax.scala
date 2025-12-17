c1db4a252bc747a87dbeeb70adcdd4d49b3c0e67
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-12-17 18:19:03 +0000

    Removed unused toSmithyTraits and toSmithy4sHints

--- modules/dynamic/src-jvm/syntax.scala
+++ modules/dynamic/src-jvm/syntax.scala
@@ -17,9 +17,9 @@
 package smithy4s.dynamic
 
 import smithy4s.{Document, ShapeId}
-import smithy4s.Document._
 import software.amazon.smithy.model.node.Node
 import software.amazon.smithy.model.shapes.{ShapeId => SmithyShapeId}
+import scala.jdk.CollectionConverters._
 
 object syntax {
   final implicit class DocumentOps(private val doc: Document) extends AnyVal {
