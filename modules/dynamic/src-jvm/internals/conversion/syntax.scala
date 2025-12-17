4d5c84760d32df70dc46f4201cf521f3fce2da3f
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-12-17 18:19:14 +0000

    Removed unused toSmithyTraits and toSmithy4sHints

--- modules/dynamic/src-jvm/internals/conversion/syntax.scala
+++ modules/dynamic/src-jvm/internals/conversion/syntax.scala
@@ -79,16 +79,4 @@ private[dynamic] object syntax {
       case _ => shape
     }
   }
-
-  private def toSmithyTraits(hints: Hints): java.util.Collection[Trait] = {
-    hints.asTraits
-  }
-
-  private def toSmithy4sHints(traits: java.util.Collection[Trait]): Hints = {
-    Hints(traits.asScala.map { t =>
-      Hints.Binding.DynamicBinding(
-        ShapeId(t.toShapeId.getNamespace, t.toShapeId.getName)
-      )
-    }.toSeq: _*)
-  }
 }
