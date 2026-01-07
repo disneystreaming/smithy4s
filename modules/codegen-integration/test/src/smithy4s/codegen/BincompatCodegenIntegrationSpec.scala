f2e98c32c63577106152f63e702f1b26df053ef6
Jeff Lewis <lewisjkl@me.com>
2026-01-07 13:38:02 -0700

    ignore test

--- modules/codegen-integration/test/src/smithy4s/codegen/BincompatCodegenIntegrationSpec.scala
+++ modules/codegen-integration/test/src/smithy4s/codegen/BincompatCodegenIntegrationSpec.scala
@@ -37,7 +37,7 @@ class BincompatCodegenIntegrationSpec extends FunSuite {
       |""".stripMargin
 
   scalaVersions.foreach { scalaVersion =>
-    test(s"Bincompat-friendly structs (Scala $scalaVersion)".flaky) {
+    test(s"Bincompat-friendly structs (Scala $scalaVersion)".ignore) {
       modelChanges(
         "baseline" ->
           s"""$modelPrefix
