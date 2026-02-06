574b72a524a67831fb9e285a122e687e2f824fc6
Antonio Morales <Antonio.Morales.-ND@disney.com>
2026-02-06 14:44:43 -0800

    fix tests and add tests

--- modules/codegen/test/src/smithy4s/codegen/transformers/AwsStandardTypesTransformerSpec.scala
+++ modules/codegen/test/src/smithy4s/codegen/transformers/AwsStandardTypesTransformerSpec.scala
@@ -218,7 +218,7 @@ final class AwsStandardTypesTransformerSpec extends munit.FunSuite {
         |  private def make(i: Int): TestStructure = TestStructure(i)
         |
         |  implicit val schema: Schema[TestStructure] = struct(
-        |    int.field[TestStructure]("i", _.i).addHints(Hints.dynamic(ShapeId("smithy.api", "default"), smithy4s.Document.fromDouble(5.0d))),
+        |    int.field[TestStructure]("i", _.i).addHints(Hints.dynamic(ShapeId("smithy.api", "default"), smithy4s.Document.fromLong(5))),
         |  )(make).withId(id).addHints(hints)
         |}""".stripMargin
     )
