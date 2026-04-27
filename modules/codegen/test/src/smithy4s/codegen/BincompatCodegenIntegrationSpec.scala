4e5f506ff291efbda8cdafa2d044455c4a40f12a
Michal Pawlik <admin@michalp.net>
2026-04-27 12:51:12 +0200

    bind codegen integration spec to smithy 0.18

--- modules/codegen/test/src/smithy4s/codegen/BincompatCodegenIntegrationSpec.scala
+++ modules/codegen/test/src/smithy4s/codegen/BincompatCodegenIntegrationSpec.scala
@@ -749,7 +749,7 @@ class BincompatCodegenIntegrationSpec extends FunSuite {
   private val smithy4sCoreDependency =
     // We're using a mutable version instead of BuildInfo
     // because we don't want a circular dependency - these tests support the codegen module, which core itself is generated with.
-    s"${BuildInfo.smithy4sOrg}::smithy4s-core:latest.stable"
+    s"${BuildInfo.smithy4sOrg}::smithy4s-core:0.18.+"
 
 // polyfill for Scala 2.12
   private implicit class PipeOps[A](private val self: A) {
