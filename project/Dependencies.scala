156e6f5c521cabd99eb9709e9d916a856df2e2b1
Yisrael Union <ysrlunion@gmail.com>
2026-07-09 11:44:57 -0400

    switch over to use interfaces library to make life better (#1977)
    (cherry picked from commit 999124c0e965d2f1bcb48e5e2db8f5577df3aa0d)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
 
   val Smithy = new {
     val org = "software.amazon.smithy"
-    val smithyVersion = "1.66.0"
+    val smithyVersion = "1.72.0"
     val model = org % "smithy-model" % smithyVersion
     val testTraits = org % "smithy-protocol-test-traits" % smithyVersion
     val build = org % "smithy-build" % smithyVersion
@@ -171,6 +171,10 @@ object Dependencies {
 
   object Mima {
     val core = "com.typesafe" %% "mima-core" % "1.1.5"
+  }
+
+  object CoursierInterface {
+    val core = "io.get-coursier" % "interface" % "1.0.29-M4"
   }
 
 }
