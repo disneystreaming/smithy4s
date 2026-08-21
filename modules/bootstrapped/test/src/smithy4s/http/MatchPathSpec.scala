0dd0ac054ea1bf2e0548efa83558aaba12f8aa6e
sshevchenko <sshevchenko@evolution.com>
2026-08-21 23:47:20 +0200

    Fix greedy label matching when followed by a static segment
    Routes like /base/{foo+}/end always 404'd since greedyAcc was
    never populated except when the greedy segment was last.
    
    Fixes #1997

--- modules/bootstrapped/test/src/smithy4s/http/MatchPathSpec.scala
+++ modules/bootstrapped/test/src/smithy4s/http/MatchPathSpec.scala
@@ -87,6 +87,36 @@ class MatchPathSpec() extends munit.FunSuite with munit.ScalaCheckSuite {
     expect.eql(result, Some(expected))
   }
 
+  test("Allows for greedy labels followed by a static segment") {
+    // /base/{foo*}/end
+    val path: List[PathSegment] =
+      List(
+        PathSegment.static("base"),
+        PathSegment.greedy("foo"),
+        PathSegment.static("end")
+      )
+    val expected = Map("foo" -> "a/b")
+    val result = doMatch(path)("base", "a", "b", "end")
+
+    expect.eql(result, Some(expected))
+  }
+
+  test(
+    "Greedy labels capture segments equal to the trailing static segment"
+  ) {
+    // /base/{foo*}/end
+    val path: List[PathSegment] =
+      List(
+        PathSegment.static("base"),
+        PathSegment.greedy("foo"),
+        PathSegment.static("end")
+      )
+    val expected = Map("foo" -> "a/end/b")
+    val result = doMatch(path)("base", "a", "end", "b", "end")
+
+    expect.eql(result, Some(expected))
+  }
+
   test("Match several segments") {
     // /{foo}/bar/{baz}
     val path: List[PathSegment] =
